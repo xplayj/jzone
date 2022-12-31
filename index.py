d6dc039b4fa25b13a3c5c059d7745d73906dee9b
BOUKA josias <81630398+xplayj@users.noreply.github.com>
2022-12-31 03:20:11 +0100

    Update index.py

--- index.py
+++ index.py
@@ -72,5 +72,5 @@ def contact():
 #         connection.sendmail(OWN_EMAIL, email_message)
 
 if __name__=='__main__':
-	app.run(debug=True)
-	# app.run(debug=False, host='0.0.0.0')
+	# app.run(debug=True)
+	app.run(debug=False, host='0.0.0.0')
